7f2997177cc5539cb4ecd4b8a39df8b549f95230
512391 <512391@bsd48.org>
2024-12-19 13:38:25 -0800

    Update Student.h

--- Student.h
+++ Student.h
@@ -1,16 +1,25 @@
 #include<iostream>
 
+
+/*
+  author: Jay Williamson
+  date: 12/19/2024
+  this class holds the data for the student
+ */
+
 namespace StudentTools
 {
   class Student
   {
     public:
+//info
     char firstName[20];
     char lastName[20];
     float GPA;
     int id;
     int lastNameLength;
     int firstNameLength;
+//seed for hashing
     int randomSeed;
     Student();
   };
