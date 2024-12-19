5b1ee0db6b288a327863d320a9d17a1d38efed66
512391 <512391@bsd48.org>
2024-12-19 13:43:35 -0800

    Update StudentGenerator.cpp

--- StudentGenerator.cpp
+++ StudentGenerator.cpp
@@ -5,35 +5,49 @@
 #include<cstring>
 #include<ctime>
 
+
+/*
+  author: Jay Williamson
+  date: 12/19/2024
+  this class generates students randomly and returns them
+ */
+
 using namespace std;
 using namespace LinkedList;
 using namespace StudentTools;
 
+//constructor for random students struct to return the students
 RandomStudents::RandomStudents(int amount)
 {
   RandomStudents::nodes = new Node*[amount];
 }
 
+//constructor for the student generator
 StudentGenerator::StudentGenerator()
 {
+//gets teh first and last names
   ifstream firstNamesFile("FirstNames.txt");
     ifstream lastNamesFile("LastNames.txt");
     StudentGenerator::currentId = 0;
 
     cout << endl;
-    
+
+//makes new arrays for the names
     for(int i = 0; i < 20; i++)
       {
 	StudentGenerator::firstNames[i] = new char[21];
 	StudentGenerator::lastNames[i] = new char[21];
       }
-    
+
+//this reads all of the first names
   if(firstNamesFile.is_open())
          {
 	   int i = 0;
 	   int j = 0;
+	//gets the current letter
            while(firstNamesFile.get(StudentGenerator::firstNames[i][j]))
 	     { 
+		//checks if it should move to the next name
 	       if(StudentGenerator::firstNames[i][j] == '\n')
 	         {
 		   i++;
@@ -45,14 +59,15 @@ StudentGenerator::StudentGenerator()
 		 }
 	     }
          }
-
+//this reads all of the last names
   if(lastNamesFile.is_open())
          {
            int i = 0;
            int j = 0;
-           
+           //gets current letter
            while(lastNamesFile.get(StudentGenerator::lastNames[i][j]))
              {
+		//checks if it should move to next name
                if(StudentGenerator::lastNames[i][j] == '\n')
                  {
                    i++;
@@ -64,6 +79,7 @@ StudentGenerator::StudentGenerator()
                  }
              }
 	 }
+//this outputs the names initially so you can see them all
   for(int i = 0; i < 20; i++)
       {
         for(int j = 0; j < 20; j++)
@@ -83,25 +99,31 @@ for(int i = 0; i < 20; i++)
 
 }
 
+//this makes random students
 RandomStudents* StudentGenerator::makeStudents(int amount)
 {
+//seeds the random
   srand(time(0));
-  
+//makes the return object of students
   RandomStudents* randomStudents = new RandomStudents(amount);
-
+//makes the amount fo students needed
   for(int i = 0; i < amount; i++)
     {
+//gets index and length of names
       int firstNameLength = rand() % 20 + 1;
       int secondNameLength = rand() % 20 + 1;
-      
+
+//this inputs the names to the student
       char firstName[20] = {};
       strncpy(firstName, StudentGenerator::firstNames[firstNameLength-1], firstNameLength);
       char lastName[20] = {};
       strncpy(lastName, StudentGenerator::lastNames[secondNameLength-1], secondNameLength);
+//randomizes other aspects about the student
        float GPA = (float)(rand()%4);
        int id = StudentGenerator::currentId;
        int randomSeed = rand() % 100 + 47;
 
+//increases the id
        StudentGenerator::currentId++;
        
        //creates the new students
@@ -122,6 +144,7 @@ RandomStudents* StudentGenerator::makeStudents(int amount)
   return randomStudents;
 }
 
+//gets the hash value for the student
 int StudentGenerator::hashStudent(Student* student, int maxNum)
 {
   int hash = (student->id * student->randomSeed) % (maxNum+1);
