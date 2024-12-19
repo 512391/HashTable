ddfae86af572dfb63703e3bdf4b13c8183058ef3
512391 <512391@bsd48.org>
2024-12-19 13:37:49 -0800

    Update Student.cpp

--- Student.cpp
+++ Student.cpp
@@ -1,6 +1,12 @@
 #include<iostream>
 #include"Student.h"
 
+/*
+  author: Jay Williamson
+  date: 12/19/2024
+  this class implements the student
+ */
+
 using namespace std;
 using namespace StudentTools;
 Student::Student()
